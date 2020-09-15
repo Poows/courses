421ac47dd8223e4a7ac5dfbfd5c05ffd80175ab5
WolfRa <sasha.akim@bk.ru>
2020-09-15 16:04:46 +0300

    add element_reference

---
+++ coursera_c++/Yellow_belt/element_reference/element_reference/element_reference.cpp
@@ -0,0 +1,35 @@
+﻿// element_reference.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+template<class key, class value> value& GetRefStrict(map<key, value>& m, key k);
+
+template<class key, class value>
+value& GetRefStrict(map<key, value>& m, key k) {
+	if (m.find(k) == m.end()) {
+		throw runtime_error("runtime_error");
+	}
+	else {
+		value& r = m[k];
+		return r;
+	}
+}
+
+//int main()
+//{
+//	try {
+//		map<int, string> m = { {0, "value"} };
+//		string& item = GetRefStrict(m, 0);
+//		item = "newvalue";
+//		cout << m[0] << endl; // выведет newvalue
+//	}
+//	catch (exception ex) {
+//		cout << ex.what() << endl;
+//	}
+//}
+
+
